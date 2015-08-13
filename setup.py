c09fdedf38c777d190a6edcc1db25b6f260bf486
Eric Dill <edill@bnl.gov>
2015-08-13 16:49:26 -0400

    conda-server is not a real package

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     author_email='srossross@gmail.com',
     url='http://github.com/Binstar/binstar_client',
     packages=find_packages(),
-    install_requires=['conda-server',
+    install_requires=['binstar',
                       'jinja2', 'psutil'],
 
     include_package_data=True,
